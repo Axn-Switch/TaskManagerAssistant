3caa2ee7fbc495af77cbb976068114ecfa570125
Axn-Switch <dietespifftimothyswift@gmail.com>
2022-08-07 23:03:46 +0100

    added toggle reminder feature

--- src/App.js
+++ src/App.js
@@ -24,11 +24,15 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  //Toggle Reminder
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
 
   return (
     <div className="container">
       <Header title="Task Manager"/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>): (<span>No Tasks to show</span>) }
     </div>
   );
 }

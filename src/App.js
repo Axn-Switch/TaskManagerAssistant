3e28911fadee56ae19f7b404dcfd9739b0d5fe3a
Axn-Switch <dietespifftimothyswift@gmail.com>
2022-08-08 23:06:00 +0100

    addtasks implemented for forms

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ function App() {
       "reminder": true
     },
   ])
+  //Add Task 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask ])
+  }
   
   //Delete Task
   const deleteTask = (id) =>{
@@ -33,7 +39,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Manager"/>
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>): (<span>No Tasks to show</span>) }
     </div>
   );

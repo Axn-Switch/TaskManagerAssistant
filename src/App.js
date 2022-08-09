47842c072c58f65d931227857702a9e1e8ba9048
Axn-Switch <dietespifftimothyswift@gmail.com>
2022-08-09 22:45:33 +0100

    added toggling feature for the form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() { 
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState(
     [{
       "id": 1,
@@ -39,7 +41,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Manager"/>
-      <AddTask onAdd={addTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}  
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>): (<span>No Tasks to show</span>) }
     </div>
   );
